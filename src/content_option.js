dd6c61d39ee4594217a183bf877937d598332f9e
wadqwd <xue15731030958@163.com>
2024-06-04 16:50:23 +0800

    Update content_option.js

--- src/content_option.js
+++ src/content_option.js
@@ -14,14 +14,14 @@ const introdata = {
   },
   description:
     "Currently a junior at Hebei University-Central Lancashire College of Media and Creativity, majoring in advertising and business management. I am willing to use what I have learned to develop together with the company!",
-  your_img_url: "http://imgagec.test.upcdn.net/2d1d0114daf3fd713fab2b1c0b6c7a5.jpg",
+  your_img_url: "http://img.liuxiaohai.ren/2d1d0114daf3fd713fab2b1c0b6c7a5.jpg",
 };
 
 const dataabout = {
   title: "JoeyXue",
   aboutme:
     "I am a creative advertising designer and currently live in China Majoring in advertising, he is currently a junior at Hebei University-Central Lancashire College of Media and Creativity. CET-4 for English majors, CET-6 or above for college English, proficient in listening, speaking, reading and writing in English. During his studies, he majored in advertising and business management. I have won many school awards, and I am willing to use what I have learned to develop together with the company!",
-  my_resume: "http://imgagec.test.upcdn.net/2d1d0114daf3fd713fab2b1c0b6c7a5.jpg",
+  my_resume: "http://img.liuxiaohai.ren/2d1d0114daf3fd713fab2b1c0b6c7a5.jpg",
 };
 const worktimeline = [
   {
@@ -88,158 +88,158 @@ const services = [
 
 const dataportfolio = [
   {
-    img: "http://imgagec.test.upcdn.net/6a72884e1f6e4b1dfcbee8b21f0c9fd.jpg",
+    img: "http://img.liuxiaohai.ren/6a72884e1f6e4b1dfcbee8b21f0c9fd.jpg",
     description: "Peripheral design",
     link: "ip",
   },
   {
-    img: "http://imgagec.test.upcdn.net/a88ff222d2956cc823c93432e3a0dd4.jpg",
+    img: "http://img.liuxiaohai.ren/a88ff222d2956cc823c93432e3a0dd4.jpg",
     description: "portfolio style",
     link: "portfolios",
   },
   {
-    img: "http://imgagec.test.upcdn.net/1.png",
+    img: "http://img.liuxiaohai.ren/1.png",
     description: "web",
     link: "webs",
   },
   {
-    img: "http://imgagec.test.upcdn.net/5c23043bd7149657e5af69058fa7453.jpg",
+    img: "http://img.liuxiaohai.ren/5c23043bd7149657e5af69058fa7453.jpg",
     description: "Packaging design",
     link: "baozhuang",
   },
   {
-    img: "http://imgagec.test.upcdn.net/5e7af0c36653ed3c416ea0ce4ca0774.jpg",
+    img: "http://img.liuxiaohai.ren/5e7af0c36653ed3c416ea0ce4ca0774.jpg",
     description: "Graphic design",
     link: "pingmian",
   },
   {
-    img: "http://imgagec.test.upcdn.net/图片3.png",
+    img: "http://img.liuxiaohai.ren/图片3.png",
     description: "Mini program",
     link: "xiaochengxu",
   },
 ];
 const xiaochengxu = [
-  { img: "http://imgagec.test.upcdn.net/图片3.png" },
-  { img: "http://imgagec.test.upcdn.net/图片4.png" },
-  { img: "http://imgagec.test.upcdn.net/图片5.png" },
-  { img: "http://imgagec.test.upcdn.net/图片6.png" },
+  { img: "http://img.liuxiaohai.ren/图片3.png" },
+  { img: "http://img.liuxiaohai.ren/图片4.png" },
+  { img: "http://img.liuxiaohai.ren/图片5.png" },
+  { img: "http://img.liuxiaohai.ren/图片6.png" },
 ];
 const baozhuang = [
-  { img: "http://imgagec.test.upcdn.net/5c23043bd7149657e5af69058fa7453.jpg" },
-  { img: "http://imgagec.test.upcdn.net/49cc4e6340c3d37ec380c03c39d8f1d.jpg" },
-  { img: "http://imgagec.test.upcdn.net/52d65d3c55d64af19be08f620a18d9c.jpg" },
-  { img: "http://imgagec.test.upcdn.net/52ef7f89cb0db2dad4b76fa1e49533c.jpg" },
-  { img: "http://imgagec.test.upcdn.net/22350bc39156b532bf2461372ccb2e8.jpg" },
-  { img: "http://imgagec.test.upcdn.net/a354091c67dca819eecdb87f3749779.jpg" },
-  { img: "http://imgagec.test.upcdn.net/d117ff1407b2bcab04b54a37c12026d.jpg" },
-  { img: "http://imgagec.test.upcdn.net/de47a458d8443f6c5d1c7daea2b942e.jpg" },
-  { img: "http://imgagec.test.upcdn.net/f7d52d0cc2149b471f1b583e36dd830.jpg" },
+  { img: "http://img.liuxiaohai.ren/5c23043bd7149657e5af69058fa7453.jpg" },
+  { img: "http://img.liuxiaohai.ren/49cc4e6340c3d37ec380c03c39d8f1d.jpg" },
+  { img: "http://img.liuxiaohai.ren/52d65d3c55d64af19be08f620a18d9c.jpg" },
+  { img: "http://img.liuxiaohai.ren/52ef7f89cb0db2dad4b76fa1e49533c.jpg" },
+  { img: "http://img.liuxiaohai.ren/22350bc39156b532bf2461372ccb2e8.jpg" },
+  { img: "http://img.liuxiaohai.ren/a354091c67dca819eecdb87f3749779.jpg" },
+  { img: "http://img.liuxiaohai.ren/d117ff1407b2bcab04b54a37c12026d.jpg" },
+  { img: "http://img.liuxiaohai.ren/de47a458d8443f6c5d1c7daea2b942e.jpg" },
+  { img: "http://img.liuxiaohai.ren/f7d52d0cc2149b471f1b583e36dd830.jpg" },
 ];
 const pingmian = [
-  { img: "http://imgagec.test.upcdn.net/5e7af0c36653ed3c416ea0ce4ca0774.jpg" },
-  { img: "http://imgagec.test.upcdn.net/66ce1a1e152981186b9da1c231c031c.png" },
-  { img: "http://imgagec.test.upcdn.net/b8476449c0051df999c2c5a1576cc33.jpg" },
-  { img: "http://imgagec.test.upcdn.net/c3d26d9d0e01f27a7ef2e16f009ebf8.jpg" },
-  { img: "http://imgagec.test.upcdn.net/fa9c7ffbc7c313ef81f935ac418e9ba.png" },
-  { img: "http://imgagec.test.upcdn.net/fbdd7a41888e44e87480e388640c53f.jpg" },
-  { img: "http://imgagec.test.upcdn.net/ff73060109d1be2d60cde64d6490edb.jpg" },
+  { img: "http://img.liuxiaohai.ren/5e7af0c36653ed3c416ea0ce4ca0774.jpg" },
+  { img: "http://img.liuxiaohai.ren/66ce1a1e152981186b9da1c231c031c.png" },
+  { img: "http://img.liuxiaohai.ren/b8476449c0051df999c2c5a1576cc33.jpg" },
+  { img: "http://img.liuxiaohai.ren/c3d26d9d0e01f27a7ef2e16f009ebf8.jpg" },
+  { img: "http://img.liuxiaohai.ren/fa9c7ffbc7c313ef81f935ac418e9ba.png" },
+  { img: "http://img.liuxiaohai.ren/fbdd7a41888e44e87480e388640c53f.jpg" },
+  { img: "http://img.liuxiaohai.ren/ff73060109d1be2d60cde64d6490edb.jpg" },
 ];
 const webs = [
-  { img: "http://imgagec.test.upcdn.net/1.png" },
-  { img: "http://imgagec.test.upcdn.net/2.png" },
-  { img: "http://imgagec.test.upcdn.net/3.png" },
-  { img: "http://imgagec.test.upcdn.net/4.png" },
-  { img: "http://imgagec.test.upcdn.net/5.png" },
+  { img: "http://img.liuxiaohai.ren/1.png" },
+  { img: "http://img.liuxiaohai.ren/2.png" },
+  { img: "http://img.liuxiaohai.ren/3.png" },
+  { img: "http://img.liuxiaohai.ren/4.png" },
+  { img: "http://img.liuxiaohai.ren/5.png" },
 ];
 const ipdesign = [
   {
-    img: "http://imgagec.test.upcdn.net/6a72884e1f6e4b1dfcbee8b21f0c9fd.jpg",
+    img: "http://img.liuxiaohai.ren/6a72884e1f6e4b1dfcbee8b21f0c9fd.jpg",
 
   },
   {
-    img: "http://imgagec.test.upcdn.net/808b0a84ff28707c5570a6a7cca2ca9.jpg",
+    img: "http://img.liuxiaohai.ren/808b0a84ff28707c5570a6a7cca2ca9.jpg",
 
   },
   {
-    img: "http://imgagec.test.upcdn.net/96d1415a10f8e3cb698336f2a8bc316.jpg",
+    img: "http://img.liuxiaohai.ren/96d1415a10f8e3cb698336f2a8bc316.jpg",
 
   },
   {
-    img: "http://imgagec.test.upcdn.net/a00905c875ac4d4f96ad93d1f7a9100.jpg",
+    img: "http://img.liuxiaohai.ren/a00905c875ac4d4f96ad93d1f7a9100.jpg",
  
   },
   {
-    img: "http://imgagec.test.upcdn.net/a88ff222d2956cc823c93432e3a0dd4.jpg",
+    img: "http://img.liuxiaohai.ren/a88ff222d2956cc823c93432e3a0dd4.jpg",
  
   },
 ];
 const portfolios = [
   {
-    img: "http://imgagec.test.upcdn.net/a88ff222d2956cc823c93432e3a0dd4.jpg",
+    img: "http://img.liuxiaohai.ren/a88ff222d2956cc823c93432e3a0dd4.jpg",
 
   },
   {
-    img: "http://imgagec.test.upcdn.net/2a477bed406873aacf73c1625075d78.jpg",
+    img: "http://img.liuxiaohai.ren/2a477bed406873aacf73c1625075d78.jpg",
 
   },
   {
-    img: "http://imgagec.test.upcdn.net/3bd8d3944069596f86f2671badd88da.jpg",
+    img: "http://img.liuxiaohai.ren/3bd8d3944069596f86f2671badd88da.jpg",
 
   },
   {
-    img: "http://imgagec.test.upcdn.net/3f4fe2d4185d85a9f01031d9c4ff5a1.jpg",
+    img: "http://img.liuxiaohai.ren/3f4fe2d4185d85a9f01031d9c4ff5a1.jpg",
 
   },
   {
-    img: "http://imgagec.test.upcdn.net/6a446d9a8de207d772eddbb6e6646e5.jpg",
+    img: "http://img.liuxiaohai.ren/6a446d9a8de207d772eddbb6e6646e5.jpg",
 
   },
   {
-    img: "http://imgagec.test.upcdn.net/9f1d61523a64c8ab349ad5040d4d5d8.jpg",
+    img: "http://img.liuxiaohai.ren/9f1d61523a64c8ab349ad5040d4d5d8.jpg",
 
   },
   {
-    img: "http://imgagec.test.upcdn.net/23d5100a10c8b7aae1d2bf54e1f111a.jpg",
+    img: "http://img.liuxiaohai.ren/23d5100a10c8b7aae1d2bf54e1f111a.jpg",
 
   },
   {
-    img: "http://imgagec.test.upcdn.net/33702dc9f7eb2be01527e76cc8e9fdc.jpg",
+    img: "http://img.liuxiaohai.ren/33702dc9f7eb2be01527e76cc8e9fdc.jpg",
 
   },
   {
-    img: "http://imgagec.test.upcdn.net/140869dd3b41d237bc9cd8ebb5e4c72.jpg",
+    img: "http://img.liuxiaohai.ren/140869dd3b41d237bc9cd8ebb5e4c72.jpg",
 
   },
   {
-    img: "http://imgagec.test.upcdn.net/871343433f2c15bf24529f231114cf2.jpg",
+    img: "http://img.liuxiaohai.ren/871343433f2c15bf24529f231114cf2.jpg",
 
   },
   {
-    img: "http://imgagec.test.upcdn.net/aa736d31cf7efec831b8a0fbce111d8.jpg",
+    img: "http://img.liuxiaohai.ren/aa736d31cf7efec831b8a0fbce111d8.jpg",
 
   },
   {
-    img: "http://imgagec.test.upcdn.net/beafbdbb6be3cf10a2a2e9975832dbe.jpg",
+    img: "http://img.liuxiaohai.ren/beafbdbb6be3cf10a2a2e9975832dbe.jpg",
 
   },
   {
-    img: "http://imgagec.test.upcdn.net/cd088daa52cdc0d4b431d73e8640e0e.jpg",
+    img: "http://img.liuxiaohai.ren/cd088daa52cdc0d4b431d73e8640e0e.jpg",
 
   },
   {
-    img: "http://imgagec.test.upcdn.net/cfc4b78e1c8af77d3b2040c42674c93.jpg",
+    img: "http://img.liuxiaohai.ren/cfc4b78e1c8af77d3b2040c42674c93.jpg",
 
   },
   {
-    img: "http://imgagec.test.upcdn.net/d21cf175281b6fa5ca4b28a927c4045.jpg",
+    img: "http://img.liuxiaohai.ren/d21cf175281b6fa5ca4b28a927c4045.jpg",
 
   },
   {
-    img: "http://imgagec.test.upcdn.net/de3d44fa5bd55bbb0d330393c931fc1.jpg",
+    img: "http://img.liuxiaohai.ren/de3d44fa5bd55bbb0d330393c931fc1.jpg",
 
   },
   {
-    img: "http://imgagec.test.upcdn.net/f524b086f36c6e0ac165a923e7dbf17.jpg",
+    img: "http://img.liuxiaohai.ren/f524b086f36c6e0ac165a923e7dbf17.jpg",
 
   },
 ];
