72b0ed5d0661d63e9623a6f6c6741c8f62299bc0
wadqwd <xue15731030958@163.com>
2024-06-04 17:23:04 +0800

    Update index.js

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -31,10 +31,6 @@ export const Portfolio = () => {
             return (
               <div key={i} className="po_item">
                 <img src={data.img} alt="" />
-                <div className="content">
-                  <p>{data.description}</p>
-                  <a href={data.link}>view project</a>
-                </div>
               </div>
             );
           })}
